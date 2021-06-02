8daf916e54472f6e44d0e052cb660d76ac1ba7ca
helendansh <danshinahel@yandex.ru>
2021-06-02 15:40:06 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Map <String, String> map = new HashMap<>();
+        map.put("one", "apple");
+        map.put("two", "pear");
+        map.put("three", "plum");
+        map.put("four", "peach");
+        map.put("five", "grapes");
+        map.put("six", "banana");
+        map.put("seven", "orange");
+        map.put("eight", "pineapple");
+        map.put("nine", "watermelon");
+        map.put("ten", "mango");
+        printValues(map);
+    }
+    public static void printValues(Map<String, String>x){
+        for (Map.Entry<String, String> value : x.entrySet()) {
+            System.out.println(value.getValue());
+        }
+    }
+}
